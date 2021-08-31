8d1780e9e7a49c24bc3bac2a1d777c11a57fdaf2
7eoz <thesandrini@gmail.com>
2021-08-30 22:06:04 -0300

    fixing functionality error

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -30,7 +30,7 @@ class CoursesPage extends React.Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault();
-		this.props.dispatch(courseActions.createCourse(this.state.course));
+		this.props.createCourse(this.state.course);
 		// alert(this.state.course.title);
 	};
 
@@ -59,7 +59,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
 	courses: PropTypes.array.isRequired,
-	dispatch: PropTypes.func.isRequired,
+	createCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -68,4 +68,10 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+	return {
+		createCourse: (course) => dispatch(courseActions.createCourse(course)),
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
