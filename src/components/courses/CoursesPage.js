9ca8b338d9ef7aa2554057a142b33bf216e7984e
7eoz <thesandrini@gmail.com>
2021-08-31 01:37:28 -0300

    removing code from CoursesPage.js

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,55 +5,14 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-	//instead of using the constructor...
-	// constructor(props) {
-	// super(props)
-	// this.state = {
-	//     course: {
-	//         title: ""
-	//     }
-	// }
-	// this.handleChange = this.handleChange.bind(this)
-	// }
-
-	//...We can simply instanciate the state as a less verbose option
-	state = {
-		course: {
-			title: '',
-		},
-	};
-
-	//By using arrow funcions we don't have to bind it to the component
-	handleChange = (event) => {
-		const course = { ...this.state.course, title: event.target.value };
-		this.setState({ course: course });
-	};
-
-	handleSubmit = (event) => {
-		event.preventDefault();
-		this.props.actions.createCourse(this.state.course);
-		// alert(this.state.course.title);
-	};
-
 	render() {
-		//By setting onSubmit in the form instead of the 'Save' button, we ensure
-		//an usability good practice because users can submit by clicking the save button
-		//or hitting 'Enter' key.
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<>
 				<h2>Courses</h2>
-				<h3>Add Course</h3>
-				<input
-					type='text'
-					onChange={this.handleChange}
-					value={this.state.course.title}
-				/>
-
-				<input type='submit' value='Save' />
 				{this.props.courses.map((course) => (
 					<div key={course.title}>{course.title}</div>
 				))}
-			</form>
+			</>
 		);
 	}
 }
