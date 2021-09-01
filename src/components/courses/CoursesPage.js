eb10ccefb8746e2a5a9e2b61a676088061eb59b2
7eoz <thesandrini@gmail.com>
2021-09-01 16:54:54 -0300

    adding thunks

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,15 +3,19 @@ import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
+	componentDidMount() {
+		this.props.actions.loadCourses().catch((error) => {
+			alert('Loading courses failed: ' + error);
+		});
+	}
 	render() {
 		return (
 			<>
 				<h2>Courses</h2>
-				{this.props.courses.map((course) => (
-					<div key={course.title}>{course.title}</div>
-				))}
+				<CourseList courses={this.props.courses} />
 			</>
 		);
 	}
