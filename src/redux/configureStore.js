ca1f6ab5dd81d3fe20e0664afa51cd116aac66e2
7eoz <thesandrini@gmail.com>
2021-09-01 00:43:58 -0300

    adding thunks

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
 	//adds support for Redux dev tools
@@ -10,6 +11,6 @@ export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
 		initialState,
-		composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+		composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
 	);
 }
