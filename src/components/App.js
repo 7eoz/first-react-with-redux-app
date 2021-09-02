d071e481fe323f4e274cb91d20cdae7ebf8898fb
7eoz <thesandrini@gmail.com>
2021-09-02 11:01:32 -0300

    adding ManageCoursesPage to routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import HomePage from './home/HomePage'
-import AboutPage from './about/AboutPage'
-import Header from './common/Header'
-import PageNotFound from './PageNotFound'
-import CoursesPage from './courses/CoursesPage'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './home/HomePage';
+import AboutPage from './about/AboutPage';
+import Header from './common/Header';
+import PageNotFound from './PageNotFound';
+import CoursesPage from './courses/CoursesPage';
+import ManageCoursesPage from './courses/ManageCoursesPage';
 
 function App() {
-    return(
-        <div className="container-fluid">
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/courses" component={CoursesPage} />
-                <Route component={PageNotFound} />
-            </Switch>
-        </div>
-    )
+	return (
+		<div className='container-fluid'>
+			<Header />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/about' component={AboutPage} />
+				<Route path='/courses' component={CoursesPage} />
+				<Route path='/course/:slug' component={ManageCoursesPage} />
+				<Route component={PageNotFound} />
+			</Switch>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/courses/ManageCoursesPage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../../redux/actions/courseActions';
+import { loadAuthors } from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+
+class ManageCoursesPage extends React.Component {
+	componentDidMount() {
+		const { courses, authors, loadCourses, loadAuthors } = this.props;
+		if (courses.length === 0) {
+			loadCourses().catch((error) => {
+				alert('Loading courses failed: ' + error);
+			});
+		}
+		if (authors.length === 0) {
+			loadAuthors().catch((error) => {
+				alert('Loading authors failed: ' + error);
+			});
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<h2>Manage Course</h2>
+			</>
+		);
+	}
+}
+
+ManageCoursesPage.propTypes = {
+	authors: PropTypes.array.isRequired,
+	courses: PropTypes.array.isRequired,
+	loadCourses: PropTypes.func.isRequired,
+	loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+	return {
+		courses: state.authors,
+		authors: state.authors,
+	};
+}
+
+const mapDispatchToProps = {
+	loadCourses,
+	loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursesPage);
