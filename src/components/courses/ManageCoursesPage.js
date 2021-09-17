b73749e981ba0dcaf32bf63b134c8e4bde1626a1
7eoz <thesandrini@gmail.com>
2021-09-17 03:20:51 -0300

    adding toast to app

--- src/components/courses/ManageCoursesPage.js
+++ src/components/courses/ManageCoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import CourseForm from './CourseForm';
 import Spinner from '../common/Spinner';
 import { newCourse } from '../../../tools/mockData';
+import { toast } from 'react-toastify';
 
 function ManageCoursesPage({
 	courses,
@@ -47,6 +48,7 @@ function ManageCoursesPage({
 		event.preventDefault();
 		setSaving(true);
 		saveCourse(course).then(() => {
+			toast.success('Course saved.');
 			history.push('/courses');
 		});
 	}
