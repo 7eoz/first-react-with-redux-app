eb7656f6a78db53694036e6626b82de9ab7ea936
7eoz <thesandrini@gmail.com>
2021-09-17 01:23:04 -0300

    adding spinner for loading feedback

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
+import Spinner from '../common/Spinner';
 import { Redirect } from 'react-router-dom';
 
 class CoursesPage extends React.Component {
@@ -32,7 +33,7 @@ class CoursesPage extends React.Component {
 			<>
 				{this.state.redirectToAddCoursePage && <Redirect to='/course' />}
 				<h2>Courses</h2>
-
+				<Spinner />
 				<button
 					style={{ marginBottom: 20 }}
 					className='btn btn-primary add-course'
