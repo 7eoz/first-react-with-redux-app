6f261a5bea1212efd26a0eb66c2893627bec5db8
7eoz <thesandrini@gmail.com>
2021-09-17 01:44:41 -0300

    adding beginApiCall to courseActions and authorActions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
+import { beginApiCall } from './apiStatusActions';
 
 export function createCourse(course) {
 	return { type: types.CREATE_COURSE, course };
@@ -19,6 +20,7 @@ export function createCourseSuccess(course) {
 
 export function loadCourses() {
 	return function (dispatch) {
+		dispatch(beginApiCall);
 		return courseApi
 			.getCourses()
 			.then((courses) => {
@@ -32,6 +34,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
 	return function (dispatch, getState) {
+		dispatch(beginApiCall);
 		return courseApi
 			.saveCourse(course)
 			.then((savedCourse) => {
